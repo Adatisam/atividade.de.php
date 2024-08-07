f674e4075b30621ea437eff308eaf4a13f72c18f
Adatisam <adati.miguel@gmail.com>
2024-08-07 16:10:17 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,10 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
     <?php 
-        echo "primeira tag";
-        $nome = "Kaucz";
-        $sobrenome = "Thiago";
-        print "eu me chamo $sobrenome $nome";
+        const PAIS = 'Brasil';
+        $nome = 'Rafael';
+        $sobrenome = 'Kaucz';
+        echo "Muito prazer, $nome $sobrenome";
         date_default_timezone_set("America/Sao_Paulo");
         echo "Hoje é dia " .date("d/M/Y");
-        echo " A hora atual é " .date("G:i:s")     
+        echo " A hora atual é " .date("G:i:s");
+        print " eu moro no"  . PAIS;     
     ?>    
-</body>
-</html>
