448067a3582072c21112916dbe780a2e907b152e
Adatisam <adati.miguel@gmail.com>
2024-08-08 17:20:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,5 +6,11 @@
         date_default_timezone_set("America/Sao_Paulo");
         echo "Hoje é dia " .date("d/M/Y");
         echo " A hora atual é " .date("G:i:s");
-        print " eu moro no"  . PAIS;     
+        print " eu moro no"  . PAIS;
+
+        $num = '[63,5,88,1,2]';
+        echo "os valores são $num";  
+        
+        $nome = '[63,5,88,1,2]';
+        echo "eu gosto do \" $nome \" E ver essas gameplay!";
     ?>    
