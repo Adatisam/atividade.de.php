66e08d95f2ac3d7717d50c3295ba7092582973e9
Adatisam <adati.miguel@gmail.com>
2024-08-09 12:02:52 -0300

    att

---
+++ formularios/form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="teste.css">
+    <link rel="shortcut icon" href="imagens/logophp.png" type="image/x-icon">
+</head>
+<body>
+    <header>
+        <h1>Resultado do processamento</h1>
+    </header>
+    <?php 
+        $nom = $_REQUEST["nome"];
+        $sobrenom = $_GET["sobrenome"];
+    ?>
+</body>
+</html>
