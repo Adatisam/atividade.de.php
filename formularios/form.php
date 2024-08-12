eed125b2a8892046ad2794353bb79fc7915a670a
Adatisam <adati.miguel@gmail.com>
2024-08-12 09:06:26 -0300

    att

--- formularios/form.php
+++ formularios/form.php
@@ -12,8 +12,10 @@
         <h1>Resultado do processamento</h1>
     </header>
     <?php 
-        $nom = $_REQUEST["nome"];
-        $sobrenom = $_GET["sobrenome"];
+        $nome = $_POST["nome"] ?? "sem nome";
+        $sobrenome = $_POST["sobrenome"] ?? "desconhecido"; 
+        echo "<p>É um prazer em conhece-lo $nom $sobrenom!, Seja bem vindo ao Fórum PHP</p>";
     ?>
+    <p><a href="javascript:history.go(-1)">Voltar para a página anterior.</a></p>
 </body>
 </html>
