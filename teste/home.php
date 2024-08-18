41605fa3117c4e0402e004a62332d6ac313c86dd
Matheus Adati Miguel <adati.miguel@gmail.com>
2024-08-18 19:27:07 -0300

    att

--- teste/home.php
+++ teste/home.php
@@ -7,8 +7,12 @@
     <link rel="stylesheet" href="../formularios/teste.css">
 </head>
 <body>
+    <?php 
+        $valor1 = $_GET['valor1']?? 0;
+        $valor2 = $_GET['valor2']?? 0;
+    ?>
     <main>
-        <form action="index.php" method="get">
+        <form action="<?php echo $_SERVER['PHP_SELF']?>" method="get">
             <label for="valor1">Valor 1</label>
             <input type="number" id="valor1" name="valor1">
             <label for="valor2">Valor 2</label>
@@ -16,8 +20,12 @@
             <input type="submit" value="Somar">
         </form>
     </main>
-    <section id="resultado">
-
+    <section id="result"> 
+        <h2>Resultado da soma</h2>
+        <?php 
+            $soma = $valor1 + $valor2;
+            echo "<p>A soma entre os valores digitados é <strong>igual a $soma</strong>.</p>";
+        ?>
     </section>
 </body>
 </html>
