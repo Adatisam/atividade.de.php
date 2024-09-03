9d84a4d818ff542ff80faab40cacbf45ea49d81b
Adatisam <adati.miguel@gmail.com>
2024-09-03 11:27:45 -0300

    Update luta.php

--- poo/luta.ufc/luta.php
+++ poo/luta.ufc/luta.php
@@ -7,22 +7,22 @@
        private $aprovada;
 
        public function marcarLuta($l1, $l2) {
-            if ($l1-> getCategoria() === $l2-> getCategoria() && ($l1 != $l2)) {
-                $this-> aprovada = true;
-                $this-> desafiado = $l1;
-                $this-> desafiante = $l2;
-            } else {
-                $this-> aprovada = false;
-                $this-> desafiado = null;
-                $this-> desafiante = null;
-            }
-       }
+          if ($l1-> getCategoria() === $l2-> getCategoria() && ($l1 != $l2)) {
+               $this-> aprovada = true;
+               $this-> desafiado = $l1;
+               $this-> desafiante = $l2;
+          } else {
+               $this-> aprovada = false;
+               $this-> desafiado = null;
+               $this-> desafiante = null;
+          }
+     }
        public function lutar() {
-            if ($this-> aprovada) {
-                $this-> desafiado -> apresentar();
-                $this-> desafiante -> apresentar();
-                $vencedor = rand(0,2);
-                switch ($vencedor) {
+          if ($this-> aprovada) {
+               $this-> desafiado -> apresentar();
+               $this-> desafiante -> apresentar();
+               $vencedor = rand(0,2);
+               switch ($vencedor) {
                     case 0: //Empate
                         echo "<p>Empate!</p>";
                         $this-> desafiado -> empatarLuta();
@@ -38,35 +38,35 @@
                         $this-> desafiado -> perderLuta();
                         $this-> desafiante -> ganharLuta();
                         break;
-                }
-            } else {
-                echo "<p>A luta foi cancelada!</p>";
-            }
-       }
+               }
+          } else {
+               echo "<p>A luta foi cancelada!</p>";
+          }
+     }
 
        function getDesafiante() {
-            return $this-> desafiante;
+          return $this-> desafiante;
        }
        function setDesafiante($de) {
-            $this-> desafiante = $de;
+          $this-> desafiante = $de;
        }
        function getDesafiado() {
-            return $this-> desafiado;
+          return $this-> desafiado;
        }
        function setDesafiado($do) {
-            $this-> desafiado = $do;
+          $this-> desafiado = $do;
        }
        function getRound() {
-            return $this-> round;
+          return $this-> round;
        }
        function setRound($r) {
-            $this-> round = $r;
+          $this-> round = $r;
        }
        function getAprovada() {
-            return $this-> aprovada;
+          return $this-> aprovada;
        }
        function setAprovada($a) {
-            $this-> aprovada = $a;
+          $this-> aprovada = $a;
        }
     }
 ?>
