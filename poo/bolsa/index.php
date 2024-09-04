4b3754e6214acc4c94ecd4bc8014c1b25c21a8a7
Adatisam <adati.miguel@gmail.com>
2024-09-04 16:24:39 -0300

    att

---
+++ poo/bolsa/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bolsa</title>
+</head>
+<body>
+    <pre>
+        <?php 
+            require "pessoa.php";
+            require "visitantes.php";
+
+            $p1 = new Pessoa("Marcio", 33, "M");
+            $v1 = new visitantes();
+        ?>
+    </pre>
+</body>
+</html>
---
+++ poo/bolsa/pessoa.php
@@ -0,0 +1,29 @@
+<?php 
+    abstract class Pessoa {
+        private $nome;
+        private $idade;
+        private $sexo;
+        public final function fazerAniversario() {
+            $this->idade ++;
+        }
+
+        public function getNome() {
+            return $this-> nome;
+        }
+        public function setNome($no) {
+            $this-> nome = $no;
+        }
+        public function getIdade() {
+            return $this-> idade;
+        }
+        public function setIdade($id) {
+            $this-> idade= $id;
+        }
+        public function getSexo() {
+            return $this-> sexo;
+        }
+        public function setSexo($sx) {
+            $this-> sexo = $sx;
+        }
+    }
+?>
